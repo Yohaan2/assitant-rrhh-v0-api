a29701d11e0e9fbb24008b49301c16ee0e4c6cd3
Yohaan2 <garciayohan57@gmail.com>
2025-06-25 12:12:01 -0400

    create project structure with nest

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  await app.listen(process.env.PORT ?? 3005, async () => {
+    console.log(`Application is running on: ${await app.getUrl()}`);
+  });
+}
+bootstrap();
